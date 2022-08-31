c9c573bbe7cadd25e3bad6e6ef392c057a9a44a4
sandracioc <sandra.cioc@yahoo.com>
2022-08-31 12:03:56 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -7,9 +7,9 @@ module "vpc" {
 
     subnets = [
         {
-            subnet_name           = "gaurav-subnet"
-            subnet_ip             = "10.100.10.0/24"
-            subnet_region         = var.region
+            subnet_name      = "gaurav-subnet"
+            subnet_ip        = "10.100.10.0/24"
+            subnet_region    = var.region
         }
         ]
 }
